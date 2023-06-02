2d5e8491c4e71754774e81ebae5d32993c3c5a02
lakshmirpem <lakshmirpem20@gmail.com>
2023-06-02 12:25:24 +0530

    commit 2

---
+++ PracticeProject/src/test/java/Sample1.java
@@ -0,0 +1,12 @@
+
+public class Sample1 
+{
+public static void main(String[] args) 
+{
+	//Welcome to this application
+	//start the execution
+	//stop the execution
+	System.out.println("hi");
+	System.out.println("bye");
+}
+}
---
+++ PracticeProject/src/test/java/lakshmi/Sample1test.java
@@ -0,0 +1,10 @@
+package lakshmi;
+
+public class Sample1test 
+{
+	public static void main(String[] args) 
+	{ 
+		//welcome to testyantra 
+		System.out.println("hellojava");
+	}
+}
